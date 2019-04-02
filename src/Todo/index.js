b1223a0e9f90938b945345b5bc2acd08fb54aad8
yonatanmk <yonatanmk@gmail.com>
2019-04-01 21:17:52 -0400

    todo wip, initial state

--- src/Todo/index.js
+++ src/Todo/index.js
@@ -1,10 +1,17 @@
 import React, { useContext, useReducer } from 'react';
+import TodosContext from './context';
+import todosReducer from './reducer';
+import TodoList from './components/TodoList'
 
 export const UserContext = React.createContext()
 
 const Todo = () => {
+  const initialState = useContext(TodosContext)
+  const [state, dispatch] = useReducer(todosReducer, initialState)
   return (
-    <div>hello</div>
+    <TodosContext.Provider value={{ state, dispatch }}>
+      <TodoList />
+    </TodosContext.Provider>
   )
 }
 
